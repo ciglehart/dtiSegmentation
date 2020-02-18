bf02fa8a75ff24cab5d1f3615305aa84b55f9d90
ciglehart <35470634+ciglehart@users.noreply.github.com>
2020-02-18 16:46:03 -0700

    Add files via upload

--- createProbMaps.py
+++ createProbMaps.py
@@ -9,17 +9,25 @@ Created on Mon Feb 17 14:04:54 2020
 import os
 import numpy as np
 import nibabel as nib
-#from matplotlib import cm
 from matplotlib.colors import ListedColormap
-from scipy.stats import mode
 
-dataPath = '/home/charlesiglehart/Documents/Research/comparisonStudy/data/oldSegmentation'
-outputDir = '/home/charlesiglehart/Documents/Research/comparisonStudy/data/probMaps/'
-outputName = 'rsProbMapRegroupedCombined.nii.gz'
+dataPath = '/home/iglehartc/Documents/Main/Misc/data/qbSegmentation'
+outputDir = '/home/iglehartc/Documents/Main/Misc/data/probMaps'
+pMapOutputName = 'qbProbMapCombined.nii.gz'
+maxMapOutputName = 'qbMaxMapCombined.nii.gz'
 cases = [1,2,3,4,5,6,7,10,11,12,13,14,15,16,17,19,20,21]
-labels = np.arange(1,12)
+labels = np.arange(1,8)
 dims = [93,187,68,len(cases)]
 
+ims = np.zeros(dims)
+for i in range(len(cases)):
+    niiPath = os.path.join(dataPath,'case'+str(cases[i])+'_QB_L_TMP.nii.gz')
+    nii = nib.load(niiPath)
+    ims[:,:,:,i] = nii.dataobj.get_unscaled()
+    niiPath = os.path.join(dataPath,'case'+str(cases[i])+'_QB_R_TMP.nii.gz')
+    nii = nib.load(niiPath)
+    ims[:,:,:,i] = ims[:,:,:,i] + nii.dataobj.get_unscaled()
+    
 stcmap = np.array([[0,	0.447058823529412,	0.698039215686275],
 [0.650980392156863,	0.0235294117647059,	0.156862745098039],
 [0,	0.619607843137255,	0.450980392156863],
@@ -35,21 +43,11 @@ stcmap = np.hstack((stcmap,np.ones([stcmap.shape[0],1])))
 stcmap = ListedColormap(stcmap)
 stcmap = stcmap(np.linspace(0,1,len(labels)))
 stcmap = ListedColormap(stcmap)
-
-ims = np.zeros(dims)
-for i in range(len(cases)):
-    niiPath = os.path.join(dataPath,'case'+str(cases[i])+'_RG_L_TMP.nii.gz')
-    nii = nib.load(niiPath)
-    ims[:,:,:,i] = nii.dataobj.get_unscaled()
-    niiPath = os.path.join(dataPath,'case'+str(cases[i])+'_RG_R_TMP.nii.gz')
-    nii = nib.load(niiPath)
-    ims[:,:,:,i] = ims[:,:,:,i] + nii.dataobj.get_unscaled()
     
 indicator = np.sum(ims>0,axis=3)>0
-
 counts = np.zeros([dims[0],dims[1],dims[2],len(labels)])
 pMap = np.zeros([dims[0],dims[1],dims[2],4])
-maxMap = np.zeros([dims[0],dims[1],dims[2]])
+maxMap = np.zeros([dims[0],dims[1],dims[2],4])
 
 for i in range(dims[0]):
     for j in range(dims[1]):
@@ -61,12 +59,16 @@ for i in range(dims[0]):
                 weights = weights/np.sum(weights)
                 pMap[i,j,k,:] = np.squeeze(np.matmul(stcmap.colors.transpose(),weights))
                 pMap[i,j,k,3] = np.sum(counts[i,j,k,:])/(len(labels)*len(cases))
-#                maxMap[i,j,k] = mode(counts[i,j,k,:]>0)
-
+                maxMap[i,j,k,:] = stcmap.colors[np.argmax(counts[i,j,k,:])]
+                
 pMap[:,:,:,3] = pMap[:,:,:,3]/np.max(pMap[:,:,:,3])    
-imgOut = pMap    
+
+imgOut1 = pMap
+imgOut2 = maxMap    
 nii.header['dim'][0] = 5
 nii.header['dim'][5] = 4
 nii.header['intent_code'] = 2004
-niiOut = nib.Nifti1Image(imgOut, nii.affine, nii.header)
-nib.save(niiOut,os.path.join(outputDir,outputName))
+niiOut1 = nib.Nifti1Image(imgOut1,nii.affine, nii.header)
+niiOut2 = nib.Nifti1Image(imgOut2,nii.affine, nii.header)
+nib.save(niiOut1,os.path.join(outputDir,pMapOutputName))
+nib.save(niiOut2,os.path.join(outputDir,maxMapOutputName))
